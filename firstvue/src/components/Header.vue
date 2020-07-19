df7182793df3b5debffd9cf98b06671f6abd9e27
Garba Adeiza Fuad <garbafuad@gmail.com>
2020-07-19 13:22:01 +0100

    First Commit, many more to come

---
+++ firstvue/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+  <b-nav tabs align="center">
+    <b-nav-item disabled>FANCY QUIZ APP</b-nav-item>
+    <b-nav-item disabled>Score : 4/10</b-nav-item>
+  </b-nav>
+</div>
+</template>
